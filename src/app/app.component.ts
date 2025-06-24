d2176f648301c94fd33291ca7c0caef0e28b68ed
Abhishek Hingorani <abhishek.hingorani@ivanti.com>
2025-06-24 20:53:48 +0530

    mock login ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'digital-library';
+
+  loginAsLibrarian() {
+    alert('You expect too much! Implement login as Librarian yourself!');
+    // Add navigation logic here
+  }
+
+  loginAsUser() {
+    alert('You expect too much! Implement login as User yourself!');
+    // Add navigation logic here
+  }
 }
